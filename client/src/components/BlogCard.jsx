9988e6c19efe74193af365c4038af831ef30dd67
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-08-21 11:55:36 +0700

    fix: edit some styling in BlogCard.jsx

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -1,68 +1,63 @@
-import { useEffect, useState } from "react";
-import axios from 'axios';
+import { FaLink } from "react-icons/fa";
 
 
-const BlogCard = () => {
-
-  const [tripsData, setTripsData] = useState([]);
-
-  const fetchTripsData = async () => {
-    try {
-    const result = await axios.get("http://localhost:4001/trips?keywords=");
-    setTripsData(result.data.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-  fetchTripsData();
-}, []);
-
+const BlogCard = ({ trip }) => {
+  if (!trip) return null;
 
   return (
-    <div className="my-10 mx-10">
-      {tripsData.map(trip => (
-        <div className="trip-card" key={trip.eid}>
-          <div className="flex flex-row justify-center mx-15 my-10">
+    <div className="trip-card my-5">
+      <div className="flex flex-row justify-center mx-15 my-10">
 
-            {/* Main Image */}
-          {trip.photos[0] && (
-        <img
-          src={trip.photos[0]}
-          alt="main-trip-image"
-          className="w-[200px] h-[150px] object-cover rounded-xl mb-2"
-        />
-      )}
-
-      {/* Title , Detail & Small images */}
-      <div className="flex flex-col ml-5 mx-20">
-          <h2 className="font-semibold font-prompt">{trip.title}</h2>
-          <p className="line-clamp-1 font-prompt text-[12px]">{trip.description}</p>
-          <a href={trip.url} className="font-prompt text-blue-600 underline text-[12px]">อ่านต่อ</a>
-          {/* Tags */}
-          <div className="flex flex-row gap-2 ">
-          <p className="font-prompt text-[12px]">หมวด</p>
-          {trip.tags.map((tag, index) => (
-            <p key={index} className="font-prompt text-[12px] underline">{tag}</p>
-          ))}
-          </div>
-          <div className="flex gap-5 mt-2">
-        {trip.photos.slice(1, 4).map((photo, index) => (
+        {/* Main Image */}
+        {trip.photos[0] && (
           <img
-            key={index}
-            src={photo}
-            alt={`trip-thumbnail-${index}`}
-            className="w-15 h-15 object-cover rounded-lg"
+            src={trip.photos[0]}
+            alt="main-trip-image"
+            className="w-[200px] h-[150px] object-cover rounded-xl mb-2"
           />
-        ))}
+        )}
+
+        {/* Title, Description & Small images */}
+        <div className="flex flex-col ml-5 mx-20">
+          <a href={trip.url}>
+          <h2 className="font-semibold font-prompt hover:text-blue-500 hover:underline">{trip.title}</h2>
+          </a>
+          <p className="line-clamp-1 font-prompt text-[12px]">{trip.description}</p>
+          <a
+            href={trip.url}
+            className="font-prompt text-blue-600 hover:text-blue-800 underline text-[12px]"
+          >
+            อ่านต่อ
+          </a>
+
+          {/* Tags */}
+          <div className="flex flex-row gap-2 mt-1">
+            <p className="font-prompt text-[12px]">หมวด</p>
+            {trip.tags?.map((tag, index) => (
+              <p key={index} className="font-prompt text-[12px] underline">{tag}</p>
+            ))}
+          </div>
+
+          {/* Small images */}
+          <div className="flex flex-row justify-between">
+          <div className="flex gap-5 mt-2">
+            {trip.photos?.slice(1, 4).map((photo, index) => (
+              <img
+                key={index}
+                src={photo}
+                alt={`trip-thumbnail-${index}`}
+                className="w-15 h-15 object-cover rounded-lg"
+              />
+            ))}
+          </div>
+            <a href={trip.url}>
+            <FaLink className="h-5 w-5 mt-10 text-blue-500 hover:text-blue-800"/>
+            </a>
+            </div>
+        </div>
       </div>
-        </div>
-        </div>
-        </div>
-      ))}
     </div>
-  )
-}
+  );
+};
 
 export default BlogCard;
--- client/src/pages/SearchTripPage.jsx
+++ client/src/pages/SearchTripPage.jsx
@@ -4,18 +4,23 @@ import BlogCard from "../components/BlogCard";
 
 const SearchTripPage = () => {
   const [tripsData, setTripsData] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
-  useEffect(() => {
-    const fetchTripsData = async () => {
-      try {
-        const res = await axios.get("http://localhost:4001/trips?keywords=");
-        setTripsData(res.data.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchTripsData();
-  }, []);
+    useEffect(() => {
+      const fetchTripsData = async () => {
+        try {
+          const res = await axios.get("http://localhost:4001/trips?keywords=");
+          setTripsData(res.data.data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchTripsData();
+    }, [searchText]);
+
+    const handleChange = (e) => {
+      setSearchText(e.target.value);
+    }
 
   return (
     <div>
@@ -28,15 +33,18 @@ const SearchTripPage = () => {
             className="px-2 w-full h-full outline-none text-gray-500 bg-transparent font-prompt"
             type="text"
             placeholder="หาที่เที่ยวแล้วไปกัน"
+            value={searchText}
+            onChange={handleChange}
           />
         </div>
       </div>
-
-      <div className="my-10 mx-10">
+      {tripsData.length > 0 && (
+         <div className="my-10 mx-10">
         {tripsData.map((trip) => (
           <BlogCard key={trip.eid} trip={trip} />
         ))}
       </div>
+      )}
     </div>
   );
 };
